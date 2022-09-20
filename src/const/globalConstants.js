65e8856ff80f06cd3f90a0e2312e1adaa2018f76
MatiasGoyaCampanella <78055866+MatiasGoyaCampanella@users.noreply.github.com>
2022-09-20 15:07:14 -0300

    base de datos db:create

--- src/const/globalConstants.js
+++ src/const/globalConstants.js
@@ -1,6 +1,11 @@
-require("dotenv").config();
+// Están todas las constantes de la API en este archivo
+
+require("dotenv").config() //importa dotenv para obtener las variables de entorno
 
 module.exports = {
-    PORT: process.env.PORT || 5000
+    PORT: process.env.PORT || 5000,
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD
 
 };
