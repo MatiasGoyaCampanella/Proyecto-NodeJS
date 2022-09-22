478da79b8d3cc8691c1ca8c92d4bc04b3e4addb3
MatiasGoyaCampanella <78055866+MatiasGoyaCampanella@users.noreply.github.com>
2022-09-22 14:29:27 -0300

    Revert "base de datos db:create"
    This reverts commit 65e8856ff80f06cd3f90a0e2312e1adaa2018f76.

--- src/const/globalConstants.js
+++ src/const/globalConstants.js
@@ -1,11 +1,6 @@
-// Están todas las constantes de la API en este archivo
-
-require("dotenv").config() //importa dotenv para obtener las variables de entorno
+require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    DB_NAME: process.env.DB_NAME,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD
+    PORT: process.env.PORT || 5000
 
 };
