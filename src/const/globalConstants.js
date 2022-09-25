344d19a710c07925fd1ade80685508fffab67314
MatiasGoyaCampanella <78055866+MatiasGoyaCampanella@users.noreply.github.com>
2022-09-25 17:52:36 -0300

    ejercicio 2

--- src/const/globalConstants.js
+++ src/const/globalConstants.js
@@ -1,6 +1,11 @@
-require("dotenv").config();
+// Son todas las constantes de la API en este archivo
+
+require("dotenv").config(); // importar dotenv para obtener las variables de entorno
 
 module.exports = {
-    PORT: process.env.PORT || 5000
+    PORT: process.env.PORT || 5000, // obtener el puerto de la plicación desde el archivo .env o si no existe usar el puerto 5000
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
 
 };
